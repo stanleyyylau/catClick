c4a3cb975fb9c66954436e18e44e48cd1e3bd113
Stanley <Stanley@Stanleys-MacBook-Air.local>
2016-06-20 16:16:56 +0800

    add cat list and use two bindings, one for name, the other click event

--- js/app.js
+++ js/app.js
@@ -1,23 +1,61 @@
+var initialCats = [{
+        catName: "Rainy",
+        catUrl: "https://c2.staticflickr.com/3/2406/2177097057_df86bce3f8_n.jpg",
+        catCount: 0,
+        nickNames:['rain rain','god calling you','whatever']
+    }, {
+        catName: "Kevin",
+        catUrl: "https://c3.staticflickr.com/3/2346/2171640282_c8ca853a62_n.jpg",
+        catCount: 0,
+        nickNames:['gaven']
+    }, {
+        catName: "Holly",
+        catUrl: "https://c1.staticflickr.com/3/2602/3977203168_b9d02a0233_m.jpg",
+        catCount: 0,
+        nickNames:['holland']
+    }, {
+        catName: "Sara",
+        catUrl: "https://c2.staticflickr.com/8/7049/13244364473_7b71bc5a4f_n.jpg",
+        catCount: 0,
+        nickNames:['queen of pictures']
+    }, {
+        catName: "Sup",
+        catUrl: "https://c5.staticflickr.com/8/7786/17202266828_46e1464ef1_n.jpg",
+        catCount: 0,
+        nickNames:['do.wong']
+    }, {
+        catName: "Test",
+        catUrl: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR-mMhJoPJTLxf4xGX8Iy3_tWKMABW0O7-X4HZ9fcnDOqXvYK6eDucCcmX_",
+        catCount: 0,
+        nickNames:['oh shit','its test']
+    }
+]
+
 var ViewModel = function() {
     var self=this;
-    self.catModel = ko.observable(new Model());
+
+    this.catList = ko.observableArray([]);
+
+
+    initialCats.forEach(function(catItem){
+      self.catList.push( new Model(catItem) );
+    });
+
+    self.catModel = ko.observable(self.catList()[0]);
+
+    self.changeCurrentCat = function(beingClick){
+      self.catModel(beingClick);
+    }
     self.catClick = function() {
-        alert("test");
         self.catModel().catCount(self.catModel().catCount() + 1);
     };
 };
 
-var Model = function() {
-    this.catName = ko.observable("John");
-    this.catUrl = ko.observable("https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg");
-    this.catCount = ko.observable(0);
-    this.nickNames = ko.observableArray([{
-        catName: "pussy"
-    }, {
-        catName: "doddy"
-    }, {
-        catName: "haha"
-    }]);
+var Model = function(object) {
+    this.catName = ko.observable(object.catName);
+    this.catUrl = ko.observable(object.catUrl);
+    this.catCount = ko.observable(object.catCount);
+    this.nickNames = ko.observableArray(object.nickNames);
     this.catLevel = ko.computed(function() {
         if (this.catCount() == 3) {
             return "infant";
